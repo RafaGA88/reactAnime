7bd46dad93e0977a321d42684ccb2a522162e69d
Rafael Ghinato <rga@cesar.school>
2022-10-16 12:44:20 -0300

    Arrumando erros de unique key

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Container } from '../../styles/GlobalStyles';
 import axios from '../../services/axios';
-import { Table } from './styled';
+import { Table, Title, ContainerBlock } from './styled';
 
 export default function Home() {
   const [animes, setAnimes] = useState([]);
@@ -17,31 +17,35 @@ export default function Home() {
 
   return (
     <Container>
-      <Table>
-        <h1>Página Principal</h1>
-        <table>
-          <tr>
-            <th>Título</th>
-            <th>Data de Lançamento</th>
-            <th>Categoria</th>
-            <th>Descrição</th>
-          </tr>
-          {animes.map((anime) => (
+      <ContainerBlock>
+        <Title>Página Principal</Title>
+        <Table>
+          <thead>
             <tr>
-              <td key={String(anime.id)}>
-                <Link to={{ pathname: `/anime/${anime.id}` }}>
-                  {anime.titulo}
-                </Link>
-              </td>
-              <td key={String(anime.id)}>
-                {anime.data_lancamento.replaceAll('-', '/')}
-              </td>
-              <td key={String(anime.id)}>{anime.categoria}</td>
-              <td key={String(anime.id)}>{anime.descricao}</td>
+              <th>Título</th>
+              <th>Data de Lançamento</th>
+              <th>Categoria</th>
+              <th>Descrição</th>
             </tr>
-          ))}
-        </table>
-      </Table>
+          </thead>
+          <tbody>
+            {animes.map((anime) => (
+              <tr key={String(anime.id)}>
+                <td key={String(anime.id + 4)}>
+                  <Link to={{ pathname: `/anime/${anime.id}` }}>
+                    {anime.titulo}
+                  </Link>
+                </td>
+                <td key={String(anime.id + 3)}>
+                  {anime.data_lancamento.replaceAll('-', '/')}
+                </td>
+                <td key={String(anime.id + 1)}>{anime.categoria}</td>
+                <td key={String(anime.id + 2)}>{anime.descricao}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </ContainerBlock>
     </Container>
   );
 }
